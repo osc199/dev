d8f22bdc46a5bf816abb36fe529e12f62f02f401
osc199 <oscarsoderberg199@gmail.com>
2025-04-23 13:30:00 +0200

    fix calendar

--- api/calendar/auth.js
+++ api/calendar/auth.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const redirect_uri = 'https://v0-mai-a.vercel.app/api/calendar/callback';
   const scope = encodeURIComponent('https://www.googleapis.com/auth/calendar.readonly');
 
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&redirect_uri=${Bredirect_uri}&scope=${scope}&access_type=offline&prompt=consent`;
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}&access_type=offline&prompt=consent`;
 
   res.redirect(authUrl);
 }
