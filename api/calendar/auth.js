be49e522edeef9d9429c4e870bdd4d70f35705c4
osc199 <oscarsoderberg199@gmail.com>
2025-04-23 12:55:50 +0200

    fix auth.js

--- api/calendar/auth.js
+++ api/calendar/auth.js
@@ -2,10 +2,10 @@
 
 export default async function handler(req, res) {
   const client_id = process.env.GOOGLE_CLIENT_ID;
-  const redirect_uri = 'https://urldefense.com/v3/__https://v0-mai-a.vercel.app/api/calendar/callback__;!!PhQDkBqkFGE!g9C9rrRYt0ojwzZK4ddWctOEpTbrlOxbpLqFIsvW1yIg6LPCId8kNf_zAp5xok_DcQhdnVWwM93ErlqiEYVedA5X$ ';
-  const scope = encodeURIComponent('https://urldefense.com/v3/__https://www.googleapis.com/auth/calendar.readonly__;!!PhQDkBqkFGE!g9C9rrRYt0ojwzZK4ddWctOEpTbrlOxbpLqFIsvW1yIg6LPCId8kNf_zAp5xok_DcQhdnVWwM93ErlqiERgupA1q$ ');
+  const redirect_uri = 'https://v0-mai-a.vercel.app/api/calendar/callback';
+  const scope = encodeURIComponent('https://www.googleapis.com/auth/calendar.readonly');
 
-  const authUrl = `https://urldefense.com/v3/__https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=$*7Bclient_id*7D&redirect_uri=$*7Bredirect_uri*7D&scope=$*7Bscope*7D&access_type=offline&prompt=consent__;JSUlJSUl!!PhQDkBqkFGE!g9C9rrRYt0ojwzZK4ddWctOEpTbrlOxbpLqFIsvW1yIg6LPCId8kNf_zAp5xok_DcQhdnVWwM93ErlqiESS01wv3$ `;
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&redirect_uri=${Bredirect_uri}&scope=${scope}&access_type=offline&prompt=consent`;
 
   res.redirect(authUrl);
 }
